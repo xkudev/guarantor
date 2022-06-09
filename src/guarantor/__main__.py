3c06515a4a90968c6002b59600997c2fd613be47
Manuel Barkhau <mbarkhau@gmail.com>
2022-06-09 10:41:23 +0000

    make fmt

--- src/guarantor/__main__.py
+++ src/guarantor/__main__.py
@@ -4,22 +4,23 @@
 #
 # Copyright (c) 2022 xkudev (xkudev@pm.me) - MIT License
 # SPDX-License-Identifier: MIT
-import os
 import click
+
 import guarantor
 
 try:
     import pretty_traceback
+
     pretty_traceback.install()
 except ImportError:
-    pass    # no need to fail because of missing dev dependency
+    pass  # no need to fail because of missing dev dependency
 
 click.disable_unicode_literals_warning = True  # type: ignore[attr-defined]
 
 
 @click.group()
 def cli() -> None:
-    """guarantor cli."""
+    """CLI for guarantor."""
 
 
 @cli.command()
--- src/guarantor/main.py
+++ src/guarantor/main.py
@@ -8,14 +8,13 @@ import enum
 
 import fastapi
 import pydantic
-
 from fastapi.staticfiles import StaticFiles
 
 
 class ModelName(str, enum.Enum):
     alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+    resnet  = "resnet"
+    lenet   = "lenet"
 
 
 app = fastapi.FastAPI()
@@ -25,21 +24,21 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 async def root():
     breakpoint()
-    return {"message": "Hello World"}
+    return {'message': "Hello World"}
 
 
 @app.get("/testint/{param}")
 async def testint(param: int, q: int | None = None):
-    return {"message": param, "q": q}
+    return {'message': param, 'q': q}
 
 
 @app.get("/testenum/{param}")
 async def testenum(param: ModelName):
-    return {"message": param}
+    return {'message': param}
 
 
 class Item(pydantic.BaseModel):
-    name: str
+    name : str
     email: pydantic.EmailStr
 
 
