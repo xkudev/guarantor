a89d7f56a58a68f7d072c1855512480c41c422fd
Fabian Barkhau <f483@protonmail.com>
2022-07-17 14:02:27 +0200

    did stuff

--- src/guarantor/aofdb.py
+++ src/guarantor/aofdb.py
@@ -23,6 +23,7 @@ Goals (in order of priority):
 import enum
 import typing as typ
 import pathlib as pl
+from guarantor import crypto
 
 WIF      = typ.Any
 
@@ -34,10 +35,8 @@ Path = str
 
 
 class OpCode(enum.Enum):
-    REPLACE = "replace"
     SET = "set"
     DEL = "del"
-    # PATCH = "patch" ?
 
 
 class Operation(typ.NamedTuple):
@@ -45,12 +44,74 @@ class Operation(typ.NamedTuple):
     data  : typ.Any
 
 
-class Update(typ.NamedTuple):
+class Change(typ.NamedTuple):
     # NOTE (mb 2022-07-17): persisted
     parent   : Hash | None
     ops      : list[Operation]
     address  : str
-    signature: str
+    signature: str  # signature of {parent + ops}
+
+
+# KV-DB: hash -> {change}
+_KVDB = {}  # hash -> change
+
+
+def get_signing_hash(change: Change):
+    return crypto.deterministic_json_hash({
+        'parent': change.parent,
+        'ops': change.ops,
+    })
+
+
+def create_change(parent: Hash | None, ops : list[Operation]) -> Change:
+    signing_hash = crypto.deterministic_json_hash({
+        'parent': parent,
+        'ops': ops,
+    })
+    signature = crypto.sign(signing_hash, wif)
+    address = crypto.get_wif_address(wif)
+    return Change(
+        parent=parent,
+        ops=ops,
+        address=address,
+        signature=signature,
+    )
+
+
+def verify_change(change: Change):
+    return crypto.verfy(
+        chagnge.address,
+        change.signature,
+        get_signing_hash(change)
+    )
+
+
+def get_change_id(change: Change) -> Hash:
+    return crypto.deterministic_json_hash(change._asdict())
+
+
+def get_change(change_id: Hash):
+    return _KVDB.get(doc_id)
+
+
+def put_change(change: Change):
+    change_id = get_change_id(change)
+    if not verify_change(change):
+        raise ValueError("Invalid change!")
+    _KVDB[change_id] = change
+    return change_id
+    
+    
+def iter_change(head_change_id: Hash) -> iterator[Change]:
+    change_id = head_change_id
+    while change := get_change(change_id):
+        yield change
+        change_id = change.parent
+
+
+def build_document(head_change_id)
+    changes = list(iter_change(head_change_id))
+    # FIXME interpret ops here
 
 
 # v1: {"state": 1}
@@ -64,15 +125,15 @@ def _diff(old: Document, new: Document) -> list[Operation]:
     return [op]
 
 
-def _patch(update: Update, ) -> Document:
+def _patch(update: Change, ) -> Document:
     pass
 
 
-def _sign_update(ops: list[Operation], parent: Hash | None, wif: WIF) -> Update:
+def _sign_update(ops: list[Operation], parent: Hash | None, wif: WIF) -> Change:
     return
 
 
-def _update(doc: Hash, key: WIF) -> Update:
+def _update(doc: Hash, key: WIF) -> Change:
     # what actually happens
     pass
 
@@ -85,10 +146,10 @@ class Client:
     def __init__(self, dirpath: pl.Path, mode='r'):
         self.dirpath = dirpath
 
-    def update(self, doc: Hash, key: WIF, old_doc: Document | None) -> Update:
+    def update(self, doc: Hash, key: WIF, old_doc: Document | None) -> Change:
         pass
 
-    # def update(self, doc: Document, key: WIF, old_doc: Document | None) -> Update:
+    # def update(self, doc: Document, key: WIF, old_doc: Document | None) -> Change:
     #     # user friendly
     #     key = BTC.parse.wif(wif)
 
