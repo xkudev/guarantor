25dc23162b536e91c6b892808b3eacc3c357f9d0
Manuel Barkhau <mbarkhau@gmail.com>
2022-07-17 11:01:57 +0000

    wip

--- test/test_aofdb.py
+++ test/test_aofdb.py
@@ -4,10 +4,16 @@ import typing as typ
 import pathlib as pl
 
 from guarantor import aofdb
+from guarantor import crypto
 from pycoin.symbols.btc import network as BTC
 
 
 import pytest
+
+FIXTURE_KEY =   crypto.KeyPair(
+    wif="L4gXBvYrXHo59HLeyem94D9yLpRkURCHmCwQtPuWW9m6o1X8p8sp",
+    addr="1LsPb3D1o1Z7CzEt1kv5QVxErfqzXxaZXv",
+)
 
 @pytest.fixture()
 def db_client(tmpdir) -> typ.Iterator[aofdb.Client]:
@@ -15,9 +21,10 @@ def db_client(tmpdir) -> typ.Iterator[aofdb.Client]:
 
 
 def test_basic(db_client: aofdb.Client):
-    pass
-    # wif, right_addr = (
-    #     '5KYZdUEo39z3FPrtuX2QbbwGnNP5zTd7yyr2SC1j299sBCnWjss',
-    #     '1HZwkjkeaoZfTSaJxDw6aKkxp45agDiEzN',
-    # ),
-    #     key = BTC.parse.wif(wif)
+    raw_doc = {"state": 1}
+    aof_doc = db_client.update(raw_doc, wif=FIXTURE_KEY.wif)
+    aof_doc = db_client.get(aof_doc['hash'])
+
+    aof_doc['state'] = 2
+    db_client.update(doc, wif=FIXTURE_KEY.wif)
+
