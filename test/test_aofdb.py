857658e9c7d2e8f14f7139c817a53934a56f3dc2
Manuel Barkhau <mbarkhau@gmail.com>
2022-07-17 14:21:32 +0000

    whoop

--- test/test_aofdb.py
+++ test/test_aofdb.py
@@ -3,28 +3,39 @@
 import typing as typ
 import pathlib as pl
 
-from guarantor import aofdb
-from guarantor import crypto
+import pytest
 from pycoin.symbols.btc import network as BTC
 
+from guarantor import aofdb
+from guarantor import crypto
 
-import pytest
-
-FIXTURE_KEY =   crypto.KeyPair(
+FIXTURE_KEY = crypto.KeyPair(
     wif="L4gXBvYrXHo59HLeyem94D9yLpRkURCHmCwQtPuWW9m6o1X8p8sp",
     addr="1LsPb3D1o1Z7CzEt1kv5QVxErfqzXxaZXv",
 )
+
 
 @pytest.fixture()
 def db_client(tmpdir) -> typ.Iterator[aofdb.Client]:
     yield aofdb.Client(pl.Path(tmpdir))
 
 
+def test_doc_diff():
+    doc_v1  = {'title': "Hello, World!"}
+    doc_v2  = {'title': "Hallo, Welt!"}
+    diff_op = aofdb.doc_diff(doc_v1, doc_v2)
+    assert aofdb.doc_patch([diff_op], doc_v1) == doc_v2
+
+
 def test_basic(db_client: aofdb.Client):
-    raw_doc = {"state": 1}
-    aof_doc = db_client.update(raw_doc, wif=FIXTURE_KEY.wif)
-    aof_doc = db_client.get(aof_doc['hash'])
+    doc_v1 = {'title': "Hello, World!"}
+    doc_v2 = {'title': "Hallo, Welt!"}
 
-    aof_doc['state'] = 2
-    db_client.update(doc, wif=FIXTURE_KEY.wif)
+    db_doc_v1 = db_client.post(doc_v1, wif=FIXTURE_KEY.wif)
+    db_doc_v2 = db_client.post(doc_v2, wif=FIXTURE_KEY.wif, prev_doc=db_doc_v1)
 
+    assert doc_v1 == db_doc_v1.raw_document
+    assert doc_v2 == db_doc_v2.raw_document
+
+    built_doc = aofdb.build_document(db_doc_v2.head_id)
+    assert built_doc == db_doc_v2.raw_document
