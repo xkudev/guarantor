81fe3f98e9ed33f2fddf90ca1faddc76ea99f9c7
Manuel Barkhau <mbarkhau@gmail.com>
2022-07-17 14:21:32 +0000

    whoop

--- src/guarantor/aofdb.py
+++ src/guarantor/aofdb.py
@@ -20,36 +20,54 @@ Goals (in order of priority):
 - Random access read performance
 - Search indexes
 """
-import enum
+import copy
+import json
 import typing as typ
+import logging
 import pathlib as pl
+
+import dictdiffer
+
 from guarantor import crypto
 
-WIF      = typ.Any
+logger = logging.getLogger("guarantor.aofdb")
+
+WIF = typ.Any
 
 Document = dict
 
-Hash = str
+ChangeHash = str
+
+
+class DatabaseDocument(typ.NamedTuple):
+    # ephemeral/never persisted
+    head_id     : ChangeHash
+    raw_document: Document
+
 
 Path = str
 
 
-class OpCode(enum.Enum):
-    SET = "set"
-    DEL = "del"
+OP_RESET     = "reset"
+OP_DICT_DIFF = "dictdiff"
+OP_SET       = "set"
+OP_DEL       = "del"
 
 
 class Operation(typ.NamedTuple):
-    opcode: OpCode
+    opcode: str
     data  : typ.Any
+
+
+Diff = list[Operation]
 
 
 class Change(typ.NamedTuple):
     # NOTE (mb 2022-07-17): persisted
-    parent   : Hash | None
-    ops      : list[Operation]
+    parent   : ChangeHash | None
+    op       : Operation
     address  : str
-    signature: str  # signature of {parent + ops}
+    signature: str  # signature of {parent + op}
 
 
 # KV-DB: hash -> {change}
@@ -57,103 +75,142 @@ _KVDB = {}  # hash -> change
 
 
 def get_signing_hash(change: Change):
-    return crypto.deterministic_json_hash({
-        'parent': change.parent,
-        'ops': change.ops,
-    })
+    return crypto.deterministic_json_hash(
+        {
+            'parent': change.parent,
+            'op'    : change.op._asdict(),
+        }
+    )
 
 
-def create_change(parent: Hash | None, ops : list[Operation]) -> Change:
-    signing_hash = crypto.deterministic_json_hash({
-        'parent': parent,
-        'ops': ops,
-    })
+def create_change(parent: ChangeHash | None, op: Operation, wif: str) -> Change:
+    signing_hash = crypto.deterministic_json_hash(
+        {
+            'parent': parent,
+            'op'    : op._asdict(),
+        }
+    )
     signature = crypto.sign(signing_hash, wif)
-    address = crypto.get_wif_address(wif)
+    address   = crypto.get_wif_address(wif)
     return Change(
         parent=parent,
-        ops=ops,
+        op=op,
         address=address,
         signature=signature,
     )
 
 
+class VerificationError(Exception):
+    pass
+
+
 def verify_change(change: Change):
-    return crypto.verfy(
-        chagnge.address,
-        change.signature,
-        get_signing_hash(change)
-    )
+    return crypto.verify(change.address, change.signature, get_signing_hash(change))
 
 
-def get_change_id(change: Change) -> Hash:
+def get_change_id(change: Change) -> ChangeHash:
     return crypto.deterministic_json_hash(change._asdict())
 
 
-def get_change(change_id: Hash):
-    return _KVDB.get(doc_id)
+def get_change(change_id: ChangeHash) -> Change:
+    change = _KVDB.get(change_id)
+    if verify_change(change):
+        return change
+    else:
+        raise VerificationError(change_id)
 
 
-def put_change(change: Change):
+def put_change(change: Change) -> ChangeHash:
     change_id = get_change_id(change)
-    if not verify_change(change):
+    if verify_change(change):
+        _KVDB[change_id] = change
+        return change_id
+    else:
         raise ValueError("Invalid change!")
-    _KVDB[change_id] = change
-    return change_id
-    
-    
-def iter_change(head_change_id: Hash) -> iterator[Change]:
-    change_id = head_change_id
+
+
+def iter_change(head_id: ChangeHash) -> typ.Iterator[Change]:
+    change_id = head_id
     while change := get_change(change_id):
         yield change
         change_id = change.parent
 
 
-def build_document(head_change_id)
-    changes = list(iter_change(head_change_id))
-    # FIXME interpret ops here
+def doc_patch(diff: Diff, old_doc: Document) -> Document:
+    new_doc = copy.deepcopy(old_doc)
+    for op in diff:
+        if op.opcode == OP_RESET:
+            new_doc = op.data
+        elif op.opcode == OP_DICT_DIFF:
+            new_doc = dictdiffer.patch(op.data, new_doc)
+        else:
+            errmsg = f"doc_patch not implemended for opcode={op.opcode}"
+            raise NotImplementedError(errmsg)
+
+    return new_doc
 
 
-# v1: {"state": 1}
-# v2: {"state": 2, "branch": {"subbranch": 2}}
-# ops [v1->v2]:
-#   set, .state
+def doc_diff(old: Document, new: Document) -> Operation:
+    diff = Operation(opcode=OP_RESET, data=new)
+
+    # TODO (mb 2022-07-17): Only replace if maybe_diff is
+    #       actually smaller.
+    #
+    # try:
+    #     dd_diff = list(dictdiffer.diff(old, new))
+    #     dd_diff = json.loads(json.dumps(dd_diff))
+    #     maybe_diff = Operation(opcode=OP_DICT_DIFF, data=dd_diff)
+    #     if doc_patch(maybe_diff, old) == new:
+    #         diff = maybe_diff
+    #     else:
+    #         logger.warning("dictdiffer failed")
+    # except ValueError:
+    #     raise
+
+    return diff
 
 
-def _diff(old: Document, new: Document) -> list[Operation]:
-    op = Operation(opcode=OpCode.REPLACE, new)
-    return [op]
+def build_document(head_id) -> Document:
+    full_diff: list[Operation] = []
+    for change in iter_change(head_id):
+        full_diff.append(change.op)
+        if change.op.opcode == OP_RESET:
+            # no need to go any further
+            break
 
-
-def _patch(update: Change, ) -> Document:
-    pass
-
-
-def _sign_update(ops: list[Operation], parent: Hash | None, wif: WIF) -> Change:
-    return
-
-
-def _update(doc: Hash, key: WIF) -> Change:
-    # what actually happens
-    pass
-
-
-# TODO (mb 2022-07-17): better document
-# class Document(typ.NamedTuple)
+    return doc_patch(reversed(full_diff), old_doc={})
 
 
 class Client:
     def __init__(self, dirpath: pl.Path, mode='r'):
         self.dirpath = dirpath
 
-    def update(self, doc: Hash, key: WIF, old_doc: Document | None) -> Change:
-        pass
+    def get(self, change_id: ChangeHash) -> Document:
+        return build_document(change_id)
 
-    # def update(self, doc: Document, key: WIF, old_doc: Document | None) -> Change:
+    def post(
+        self,
+        doc     : Document | DatabaseDocument,
+        wif     : str,
+        prev_doc: DatabaseDocument | None = None,
+    ) -> ChangeHash:
+        if prev_doc is None:
+            new_doc_op = Operation(opcode=OP_RESET, data=doc)
+        else:
+            new_doc_op = doc_diff(old=prev_doc.raw_document, new=doc)
+
+        parent  = (prev_doc and prev_doc.head_id) or None
+        change  = create_change(parent=parent, op=new_doc_op, wif=wif)
+        head_id = put_change(change)
+        return DatabaseDocument(head_id, copy.deepcopy(doc))
+
+    # def find(self, query: Query) -> typ.Iterator[]:
+
+    # def update(self, doc: Document, wif: WIF, old_doc: Document | None) -> Change:
     #     # user friendly
     #     key = BTC.parse.wif(wif)
 
     #     pass
 
-    def _append(self, op: Operation) -> None:
+    def _append(self, diff: Operation) -> None:
         pass
