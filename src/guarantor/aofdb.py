0634d4861feca6d8bbb265aa07dd362d12179baa
Manuel Barkhau <mbarkhau@gmail.com>
2022-07-17 11:01:57 +0000

    wip

--- src/guarantor/aofdb.py
+++ src/guarantor/aofdb.py
@@ -9,12 +9,12 @@
 Non-Goals:
 - Durability
     - Durability is not provided at the db level but via
-      replication of the AOL.
+      replication of the AOF.
 - Write Performance
 
 Goals (in order of priority):
 - Consistency (Correctness):
-    - We use AOL: append only log/file which we replicate and replay
+    - We use AOF: append only log/file which we replicate and replay
 - Encapsulate signing of document operations
 - One writer, many readers.
 - Random access read performance
@@ -24,35 +24,75 @@ import enum
 import typing as typ
 import pathlib as pl
 
+WIF      = typ.Any
+
+Document = dict
+
+Hash = str
+
+Path = str
+
 
 class OpCode(enum.Enum):
+    REPLACE = "replace"
     SET = "set"
     DEL = "del"
-    # PATCH = "patch"
+    # PATCH = "patch" ?
 
 
 class Operation(typ.NamedTuple):
-    opcode  : OpCode
-    doc_path: str
-    data    : typ.Any
+    opcode: OpCode
+    data  : typ.Any
 
 
 class Update(typ.NamedTuple):
-    ops: list[Operation]
+    # NOTE (mb 2022-07-17): persisted
+    parent   : Hash | None
+    ops      : list[Operation]
+    address  : str
+    signature: str
+
+
+# v1: {"state": 1}
+# v2: {"state": 2, "branch": {"subbranch": 2}}
+# ops [v1->v2]:
+#   set, .state
+
+
+def _diff(old: Document, new: Document) -> list[Operation]:
+    op = Operation(opcode=OpCode.REPLACE, new)
+    return [op]
+
+
+def _patch(update: Update, ) -> Document:
+    pass
+
+
+def _sign_update(ops: list[Operation], parent: Hash | None, wif: WIF) -> Update:
+    return
+
+
+def _update(doc: Hash, key: WIF) -> Update:
+    # what actually happens
+    pass
 
 
 # TODO (mb 2022-07-17): better document
 # class Document(typ.NamedTuple)
-Document = dict
-WIF      = typ.Any
 
 
 class Client:
     def __init__(self, dirpath: pl.Path, mode='r'):
         self.dirpath = dirpath
 
-    def update(self, doc: Document, key: WIF, old_doc: Document | None) -> Operation:
+    def update(self, doc: Hash, key: WIF, old_doc: Document | None) -> Update:
         pass
+
+    # def update(self, doc: Document, key: WIF, old_doc: Document | None) -> Update:
+    #     # user friendly
+    #     key = BTC.parse.wif(wif)
+
+    #     pass
 
     def _append(self, op: Operation) -> None:
         pass
