58c6a1bbe56a2541ba005a2d02833fb2636951fe
Manuel Barkhau <mbarkhau@gmail.com>
2022-08-19 14:19:40 +0000

    add missing cli module

---
+++ src/guarantor/cli.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# This file is part of the guarantor project
+# https://github.com/xkudev/guarantor
+#
+# Copyright (c) 2022 xkudev (xkudev@pm.me) - MIT License
+# SPDX-License-Identifier: MIT
+import logging
+
+import click
+
+try:
+    import pretty_traceback
+
+    pretty_traceback.install()
+except ImportError:
+    pass  # no need to fail because of missing dev dependency
+
+
+logger = logging.getLogger("guarantor.cli")
+
+
+@click.group(context_settings={'help_option_names': ["-h", "--help"]})
+@click.version_option(version="2022.1001-alpha")
+def cli() -> None:
+    """CLI for guarantor."""
