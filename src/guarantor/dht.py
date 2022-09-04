20603427c368bd33bcb8cb390a8c42759bc5bb80
Fabian Barkhau <f483@protonmail.com>
2022-09-04 16:12:00 +0200

    added get_changes to storage

--- src/guarantor/dht.py
+++ src/guarantor/dht.py
@@ -1,9 +1,13 @@
 import random
+import asyncio
 import logging
 import binascii
 
+from kademlia.node import Node
 from kademlia.utils import digest
+from kademlia.network import Server
 from kademlia.storage import ForgetfulStorage
+from kademlia.crawling import NodeSpiderCrawl
 
 from guarantor import schemas
 
@@ -26,10 +30,61 @@ def get_distance(digest_a, digest_b) -> int:
     return bin_to_int(digest_a) ^ bin_to_int(digest_b)
 
 
+def load_change(key, value):
+    try:
+        change = schemas.loads_change(value)
+
+        if digest(change.change_id) != key:
+            logger.warning(f"Change key missmatch: {digest(change.change_id)} != {key}")
+            return None
+
+        return change
+
+    except schemas.VerificationError:
+        logger.warning(f"Invalid change: {value}")
+        return None
+
+
+class ChangeServer(Server):
+    async def set_digest(self, dkey, value):
+        result = await super().set_digest(dkey=dkey, value=value)
+
+        # Modify the set_digest method to additionally
+        # store changes close to the change.address digest.
+        # This enables finding all changes for a giving address.
+
+        change = load_change(dkey, value)
+        if not change:
+            return result
+
+        address_digest = digest(change.address)
+        node           = Node(address_digest)
+
+        # code below if copied verbatim from Server.set_digest
+
+        nearest = self.protocol.router.find_neighbors(node)
+        if not nearest:
+            logger.warning("There are no known neighbors to set key %s", dkey.hex())
+            return False
+
+        spider = NodeSpiderCrawl(self.protocol, node, nearest, self.ksize, self.alpha)
+        nodes  = await spider.find()
+        logger.info("setting '%s' on %s", dkey.hex(), list(map(str, nodes)))
+
+        # if this node is close too, then store here as well
+        biggest = max([n.distance_to(node) for n in nodes])
+        if self.node.distance_to(node) < biggest:
+            self.storage[dkey] = value
+        results = [self.protocol.call_store(n, dkey, value) for n in nodes]
+        # return true only if at least one store call succeeded
+        return any(await asyncio.gather(*results)) or result
+
+
 class ChangeStorage(ForgetfulStorage):
-    def __init__(self, ttl=604800, max_entries=1000000, node_id=None):
+    def __init__(self, ksize=20, ttl=604800, max_entries=1000000, node_id=None):
         super().__init__(ttl=ttl)
 
+        self.ksize       = ksize
         self.max_entries = max_entries
         self.node_id     = node_id  # needed for value metric
         assert self.node_id is not None, "Missing required node_id!"
@@ -37,18 +92,30 @@ class ChangeStorage(ForgetfulStorage):
     def __setitem__(self, key, value):
 
         # drop invalid changes
-        try:
-            change = schemas.loads_change(value)
-
-            if digest(change.change_id) != key:
-                logger.warning(f"Change key missmatch: {digest(change.change_id)} != {key}")
-                return
-
-        except schemas.VerificationError:
-            logger.warning(f"Invalid change: {value}")
+        if not load_change(key, value):
             return
 
         super().__setitem__(key, value)
+
+    def get_changes(self, address_digest, after_key=None):
+
+        keys = []
+        for key, pair in self.data.items():
+            _, value = pair
+
+            change = load_change(key, value)
+            if not change:
+                continue
+            if address_digest == digest(change.address):
+                keys.append(key)
+
+        keys.sort()
+
+        try:
+            key_index = keys.index(after_key)
+            return keys[key_index + 1 : key_index + 1 + self.ksize]
+        except ValueError:
+            return keys[: self.ksize]
 
     def cull(self):
         entries = []
--- test/test_dht.py
+++ test/test_dht.py
@@ -19,6 +19,83 @@ def test_get_distance():
     assert distance == expected
 
 
+def test_get_changes_after_key():
+    node_id = dht.generate_node_id()
+    storage = dht.ChangeStorage(node_id=node_id, ksize=20)
+    address = crypto.get_wif_address(WIF)
+
+    change_id_digests = []
+    for i in range(50):
+        change = schemas.make_change(
+            wif=WIF,
+            doctype=f"{i}",
+            opcode='bar',
+            opdata={},
+            difficulty=(i // 10) + 1,
+        )
+        change_data      = schemas.dumps_change(change)
+        change_id_digest = digest(change.change_id)
+        storage[change_id_digest] = change_data
+        change_id_digests.append(change_id_digest)
+
+    change_id_digests.sort()
+
+    after_key = change_id_digests[5]
+    expected  = change_id_digests[6 : 6 + 20]
+
+    keys = storage.get_changes(address_digest=digest(address), after_key=after_key)
+    assert set(keys) == set(expected)
+
+
+def test_get_changes_limit():
+    node_id = dht.generate_node_id()
+    storage = dht.ChangeStorage(node_id=node_id, ksize=20)
+    address = crypto.get_wif_address(WIF)
+
+    change_id_digests = []
+    for i in range(30):
+        change = schemas.make_change(
+            wif=WIF,
+            doctype=f"{i}",
+            opcode='bar',
+            opdata={},
+            difficulty=(i // 10) + 1,
+        )
+        change_data      = schemas.dumps_change(change)
+        change_id_digest = digest(change.change_id)
+        storage[change_id_digest] = change_data
+        change_id_digests.append(change_id_digest)
+
+    change_id_digests.sort()
+
+    keys = storage.get_changes(address_digest=digest(address))
+    assert keys == change_id_digests[:20]
+
+
+def test_get_changes():
+    node_id = dht.generate_node_id()
+    storage = dht.ChangeStorage(node_id=node_id)
+    address = crypto.get_wif_address(WIF)
+
+    change_id_digests = []
+    for i in range(10):
+        change = schemas.make_change(
+            wif=WIF,
+            doctype=f"{i}",
+            opcode='bar',
+            opdata={},
+            difficulty=(i // 10) + 1,
+        )
+        change_data      = schemas.dumps_change(change)
+        change_id_digest = digest(change.change_id)
+        storage[change_id_digest] = change_data
+        change_id_digests.append(change_id_digest)
+    change_id_digests.sort()
+
+    keys = storage.get_changes(address_digest=digest(address))
+    assert keys == change_id_digests
+
+
 def test_storage_cull_max_entries():
 
     node_id = dht.generate_node_id()
